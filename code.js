3a79a798a835b395d681dfd67a9a3320c6798f9c
Love <love@deurell.net>
2022-05-20 08:49:15 +0200

    final product for real

--- code.js
+++ code.js
@@ -19,3 +19,12 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
